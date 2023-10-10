3f563876dfc53425c9671fd6a44facb7a14ec73d
Bushman77 <banolik@gmail.com>
2023-10-09 22:05:23 -0700

    Added unit test checking connectivity to remote api

--- kdabet_umbrella/apps/mlbschedule/test/mlbschedule_test.exs
+++ kdabet_umbrella/apps/mlbschedule/test/mlbschedule_test.exs
@@ -3,9 +3,21 @@ defmodule MlbScheduleTest do
   doctest MlbSchedule
 
   setup_all do
-    {:ok, MlbSchedule.get_state()}
+    {:ok,
+     [
+       schedule: MlbSchedule.get_state(),
+       api:
+         MlbSchedule.MlbClient.get(
+           "/schedule/?sportId=1&date=#{Date.utc_today() |> Date.to_string()}"
+         )
+     ]}
   end
 
   test "greets the world", do: assert(MlbSchedule.hello() == :world)
-  test("MlbSchedule genserver holding a struct?", state, do: assert(is_map(state)))
+  test("MlbSchedule genserver holding a struct?", state, do: assert(is_map(state[:schedule])))
+
+  test "Can we connect to statsapi.mlb.com?", state do
+    {:ok, res} = state[:api]
+    assert res.status == 200
+  end
 end
