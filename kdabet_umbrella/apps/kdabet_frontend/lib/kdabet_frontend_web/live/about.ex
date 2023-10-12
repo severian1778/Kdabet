12603c124ca7792c04cc228dc110e28727095942
Stephen Rothwell <stevecronin11@gmail.com>
2023-10-12 02:45:23 -0700

    adding a swath of 50 kings

--- kdabet_umbrella/apps/kdabet_frontend/lib/kdabet_frontend_web/live/about.ex
+++ kdabet_umbrella/apps/kdabet_frontend/lib/kdabet_frontend_web/live/about.ex
@@ -6,9 +6,9 @@ defmodule KdabetFrontendWeb.About do
     ~F"""
     <div class="flex flex-col xl:flex-row w-full h-fit xl:h-[85vh] overflow-hidden">
       <!-- About us -->
-      <section class="fader mt-5 w-full md:w-5/6 lg:w-5/6 xl:w-1/2 justify-center m-auto xl:justify-start h-full overflow-hidden invisible-scroll overflow-y-scroll">
-        <h2 class="fade_title w-full text-center xl:text-right pb-5 text-[3em] md:text-[4em] xl:text-[5.5em] block md:hidden">ABOUT US</h2>
-        <article class="glasscard p-[30px] lg:p-[50px] mt-[20px]">
+      <section class="fader w-full md:w-5/6 lg:w-5/6 xl:w-1/2 justify-center m-auto xl:justify-start h-full overflow-hidden invisible-scroll overflow-y-scroll">
+        <h2 class="fade_title w-full mt-5 xl:mt-0 text-center xl:lg:text-left pb-5 text-[3em] md:text-[4em] xl:text-[5.5em]">ABOUT US</h2>
+        <article class="glasscard p-[30px] lg:p-[50px]">
           <h2 class="glowtext mb-8 text-2xl">KDAbet is a pure, decentralized sportsbook.</h2>
           <p class="glowtext mb-3">KDAbet is an experimental U.S sports and E-sports entertainment platform running on the Kadena L1 network.</p>
           <p class="glowtext mb-3">The KDAbet platform is the first prediction market of its kind, where the operation has no owners, board of directors, company structure or token.  The automated bookmaking system is overseen only via a decentralized autonomous organization (DAO) that collectivizes its progression over time via a fair vote.</p>
