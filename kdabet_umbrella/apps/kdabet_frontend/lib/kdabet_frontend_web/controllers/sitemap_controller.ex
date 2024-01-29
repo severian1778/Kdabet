c4fb10d6ced7852ec3481dfc1d0332db378f4c7e
Bushman77 <banolik@gmail.com>
2024-01-29 15:47:06 -0800

    Responder for google crawler services

---
+++ kdabet_umbrella/apps/kdabet_frontend/lib/kdabet_frontend_web/controllers/sitemap_controller.ex
@@ -0,0 +1,33 @@
+defmodule KdabetFrontendWeb.SitemapController do
+  use KdabetFrontendWeb, :controller
+  alias KdabetFrontendWeb.Sitemap
+
+  def index(conn, _params) do
+    conn
+    |> put_resp_content_type("text/xml")
+    |> send_resp(
+      200,
+      """
+      <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">
+      <url>
+      <loc>https://kdabet.com/about</loc>
+      <changefreq>never</changefreq>
+      </url>
+      <url>
+      <loc>https://kdabet.com/kings</loc>
+      <changefreq>never</changefreq>
+      </url>
+      <url>
+      <loc>https://kdabet.com/mint</loc>
+      <changefreq>never</changefreq>
+      </url>
+      <url>
+      <loc>https://kdabet.com/casino</loc>
+      <changefreq>never</changefreq>
+      </url>
+      </urlset>
+      """
+    )
+  end
+end
